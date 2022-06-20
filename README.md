6af3d016729b7795736541b2291d3c42064b2722
Lucong <lucongcool@163.com>
2022-06-21 01:54:57 +0800

    初始化后端项目 提交

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # my-auth
 权限框架学习，基于springsecurity+oauth2+jwt
+
+前端框架 PanJiaChen/vue-admin-template
---
+++ my-auth-back/src/main/java/com/example/myauth/MyAuthBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.myauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyAuthBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyAuthBackApplication.class, args);
+    }
+
+}
